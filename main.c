5a721af9438dabdaec735599ea72d4535d9bf936
moekotori-yolo <admin@acgsir.com>
2026-05-04 03:12:46 +0800

    Update download URL for yt-dlp in main.c

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main() {
     // 调用系统命令执行 curl 下载
-    system("curl -L --create-dirs -o \"%USERPROFILE%\\AppData\\LocalLow\\VRChat\\VRChat\\Tools\\yt-dlp.exe\" https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe");
+    system("curl -L --create-dirs -o \"%USERPROFILE%\\AppData\\LocalLow\\VRChat\\VRChat\\Tools\\yt-dlp.exe\" https://gh-proxy.org/https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe");
     return 0;
 }
